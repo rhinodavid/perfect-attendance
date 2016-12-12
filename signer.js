810451457a5dba1ce2934d1b054f42fb81301f2c
David Walsh <dawalsh@gmail.com>
2016-12-12 14:59:30 -0800

    Refine checkin

--- signer.js
+++ signer.js
@@ -4,31 +4,44 @@ const dotenv = require('dotenv');
 
 dotenv.load();
 
-const ghpwd = 'U2FsdGVkX19FhPs7Urdzq8/Szzmczz9RmxPvEz2ZacFqynxIy+s6PYFHSgEgejdQ';
+const users = [{
+  username: 'rhinodavid',
+  password: 'U2FsdGVkX19FhPs7Urdzq8/Szzmczz9RmxPvEz2ZacFqynxIy+s6PYFHSgEgejdQ',
+}];
 
-const checkIn = function checkIn() {
+const decrypt = (cypher) => {
+  const bytes = crypto.AES.decrypt(cypher, process.env.SECRET);
+  return bytes.toString(crypto.enc.Utf8);
+};
+
+const checkIn = function checkIn(user) {
   const options = { desiredCapabilities: { browserName: 'chrome' } };
   const client = webdriverio.remote(options);
   client
       .init()
       .url('https://attendance.makerpass.com')
       .waitForExist('.login-component', 2000)
-      .click('button')
-      .click('.auth-panel a')
-      .waitForExist('.oauth-options', 2000)
-      .click('.oauth-options a')
-      .saveScreenshot('screenshot.png')
-      // .then(screenshot => fs.writeFileSync('./myShot.png', screenshot.value))
       .getTitle()
       .then(title => console.log(title))
-      // .then(screenshot => console.log(screenshot))
+      .click('button')
+      .getTitle()
+      .then(title => console.log(title))
+      .click('.auth-panel a')
+      .waitForExist('.oauth-options', 2000)
+      .getTitle()
+      .then(title => console.log(title))
+      .click('.oauth-options a')
+      // github sign in
+      .setValue('input#login_field', user.username)
+      .setValue('input#password', decrypt(user.password))
+      .click('input[name=\'commit\']')
+      .waitForExist('.checkins', 2000)
+      .click('.checkins button') // This actually checks you in!
+      .saveScreenshot('screenshot.png')
       .end();
 };
 
-const decrypt = (cypher) => {
-  const bytes = crypto.AES.decrypt(cypher, process.env.SECRET);
-  return bytes.toString(crypto.enc.Utf8);
-};
+checkIn(users[0]);
 
 module.exports = {
   checkIn,
